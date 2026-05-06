b8273e82a1eeacad8fc01a60eacc611b4fb92c0b
Joaquin Zarate Sanchez <joaquin.zarate@ids.com.mx>
2026-05-06 17:06:13 -0600

    feat: Crear entidad Categoria con campos: id, nombre, descripcion

---
+++ src/main/java/com/ids/cursos/model/Categoria.java
@@ -0,0 +1,26 @@
+package com.ids.cursos.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categorias")
+
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 500)
+    private String descripcion;
+
+ 
+}
